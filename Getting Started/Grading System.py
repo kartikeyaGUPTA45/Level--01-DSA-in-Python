20d168c50c0292e4ffa9165124dc0dca3d17feda
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2022-01-23 00:47:17 +0530

    Create Grading System.py

---
+++ Getting Started/Grading System.py
@@ -0,0 +1,15 @@
+
+
+
+marks=int(input())
+
+if marks>90:
+    print("excellent")
+elif marks>80:
+    print("good")
+elif marks>70:
+    print("fair")
+elif marks>60:
+    print("meets expectations")
+else :
+    print("below par")
